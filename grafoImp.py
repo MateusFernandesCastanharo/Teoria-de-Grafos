6dc4f646b310bee29d8177fab846dc56d8f9b10d
MateusFernandesCastanharo <mateuscastanharo64@gmail.com>
2023-09-25 23:47:15 -0300

    Add files via upload

---
+++ grafoImp.py
@@ -0,0 +1,80 @@
+"""
+    Mateus Fernandes Castanharo - 32137141
+    Victor Hugo Antonio Couto - 32173482
+"""
+
+
+class Grafo:
+    # construtor da classe grafo
+
+    def __init__(self, n):
+        self.n = n  # número de vértices
+        self.m = 0  # número de arestas
+        self.typeGraph = 0
+        self.pesos = {}  # pesos de cada aresta
+        self.nomes = {}
+        # lista de adjacência
+        self.listaAdj = [[] for i in range(self.n)]
+
+
+
+    def insereVertice(self, v):
+        idVertice = self.n + 1
+        self.nomes[idVertice] = v;
+
+        for i in self.nomes:
+            if v == self.nomes[i]:
+                print("Vértice já existente.")
+                return
+        
+        self.n += 1
+        self.listaAdj.append(idVertice)
+        print("Vértice adicionado com sucesso.")
+
+
+        # Insere uma aresta no Grafo tal que
+        # v é adjacente a w
+    def insereAresta(self, v, w, peso):
+        #print(self.listaAdj)
+        # for (i,j) in self.listaAdj:
+        #     if (i,j) == (v,w):
+        #         print("Aresta existente.")
+        #         return
+
+
+        self.listaAdj[v].append(w)
+        self.listaAdj[w].append(v)
+        self.pesos[v, w] = peso     # Guarda na lista de pesos
+        self.pesos[w, v] = peso
+        self.m += 1
+
+        # Apresenta o Grafo contendo
+        # número de vértices, arestas
+        # e a LISTA de adjacência obtida
+    def show(self):
+        print(f"\n n:{self.n:2d}", end="")
+        print(f" m:{self.m:2d}")
+        for i in range(self.n):
+            if i == 0:
+                continue
+            else:
+                print(f"\n{self.nomes[i-1]}", end="")
+            for w in range(len(self.listaAdj[i])):
+                val = self.listaAdj[i][w]
+                # printa pesos de cada aresta
+                print(
+                    f" <--{self.pesos[i,val]}--> {self.nomes[val-1]}", end="")
+
+        print("\n\nfim da impressao do grafo.")
+
+    def busca_profundidade(self, vertice_inicial):
+            visitados = set()
+
+            def dfs(vertice):
+                visitados.add(vertice)
+                for vizinho in self.listaAdj[vertice]:
+                    if vizinho not in visitados:
+                        dfs(vizinho)
+
+            dfs(vertice_inicial)
+            return visitados     # retorna os vértices visitados
